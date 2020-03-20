55eb4e912fd65e881a4deb71fdc686b01f8179d5
Miguel36 <miguelmontilla.1936@gmail.com>
2020-03-19 22:50:12 -0500

    Se agrega y configura la etiqueta select en el form

---
+++ WebContent/register.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Registration Form</title>
+<s:head/>
+</head>
+<body>
+
+<h2>Registration Form</h2>
+
+<s:form action="registerAction">
+	<s:textfield name="firstName" label="First Name" />
+	<s:textfield name="lastName" label="Last Name"/>
+	<s:radio name="gender" label="Gender" list="{'Male', 'Female'}" />
+	<s:textfield name="age" label="Age" />
+	<s:textfield name="email" label="Email" />
+	<s:textarea name="address" label="Address" cols="30" rows="4" />
+	<s:select list="colors" name="selectedColor" headerKey="None" headerValue="Select a color" label="Favourite color" />	
+	<s:reset value="reset"/><s:submit value="Register" />
+</s:form>
+
+</body>
+</html>
---
+++ src/RegisterAction.java
@@ -0,0 +1,129 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import com.opensymphony.xwork2.ActionSupport;
+
+
+@SuppressWarnings("serial")
+public class RegisterAction extends ActionSupport{
+
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private Integer age;
+	private String email;
+	private String address;
+	private String selectedColor;
+	private List<String> colors;
+	
+	public String initializeFormFields() {
+		initializeColors();
+		return "input";
+	}
+	
+	private void initializeColors() {
+		colors = new ArrayList<>(
+				Arrays.asList("Blue", "Red", "Green", "White")
+		);
+		
+	}
+
+	public String execute() {
+
+		initializeFormFields();
+		System.out.println("execute() method called");
+		return "success";
+	}
+	
+	
+//	public void validate() {
+//		if (firstName == null || firstName.equals("")) {
+//			addFieldError("firstName", "First Name is required");
+//		}
+//		if (lastName == null || lastName.equals("")) {
+//			addFieldError("lastName", "Last Name is required");
+//		}
+//		
+//		if (gender == null) {
+//			addFieldError("gender", "Gender is required");
+//		}
+//		
+//		if (age == null) {
+//			addFieldError("age", "Age is required");
+//		}
+//		else if (age <= 18) {
+//			addFieldError("age", "Age should be above 18");
+//		}
+//		if (email == null || email.equals("")) {
+//			addFieldError("email", "Email is required");
+//		}
+//	}
+	
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getSelectedColor() {
+		return selectedColor;
+	}
+	
+	public void setSelectedColor(String selectedColor) {
+		this.selectedColor = selectedColor;
+	}
+	
+	public List<String> getColors() {
+		return colors;
+	}
+
+	public void setColors(List<String> colors) {
+		this.colors = colors;
+	}
+
+}
